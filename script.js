da8709250e6d5c6c034b67408aac53e28114fecb
Valentin <valentinosadchiy@gmail.com>
2020-08-23 18:52:51 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
-var element = document.getElementsByClassName("demojs") [0]
-element.addEventListener("click",function() {window.open("https://power-secret.com")})
+var element1 = document.getElementsByClassName("demojs1") [0]
+element1.addEventListener("click",function() {window.open("https://power-secret.com")})
+
+var element2 = document.getElementsByClassName("demojs2") [0]
+element2.addEventListener("click",function() {window.open("https://power-secret.com")})
+
+var element3 = document.getElementsByClassName("demojs3") [0]
+element3.addEventListener("click",function() {window.open("https://power-secret.com")})
